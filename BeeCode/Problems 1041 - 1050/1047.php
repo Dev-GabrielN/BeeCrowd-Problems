24ad89d61166ab43a178cd579e5b2358d6901787
Gabriel Nascimento de Souza <84488487+Dev-GabrielN@users.noreply.github.com>
2022-12-01 08:34:05 -0300

    Update 1047.php

--- BeeCode/Problems 1041 - 1050/1047.php
+++ BeeCode/Problems 1041 - 1050/1047.php
@@ -8,33 +8,26 @@ $inicio_minutos = intval($entrada[1]);
 $final = intval($entrada[2]);
 $final_minutos = intval($entrada[3]);
 
-if ($inicio <= $final && $inicio_minutos != $final_minutos) {
-    $duracao = ($final - $inicio);
-    $duracao_minutos = ($final_minutos - $inicio_minutos);
+$total = ($final - $inicio);
+$total_minutos = ($final_minutos - $inicio_minutos);
 
-    if ($inicio == $final && $duracao_minutos < 0) {
-        $duracao = 24;
-        $duracao = $duracao - 1;
-        $duracao_minutos = 60 + ($duracao_minutos);
 
-        echo "O JOGO DUROU $duracao HORA(S) E $duracao_minutos MINUTO(S)\n";
-    } else {
-        echo "O JOGO DUROU $duracao HORA(S) E $duracao_minutos MINUTO(S)\n";
-    }
+
+if ($total <= 0) {
+    $total = 24 + ($total);
 }
 
-if ($inicio <= $final && $inicio_minutos == $final_minutos){
-    $duracao = $final - $inicio;
-    $duracao_minutos = 0;
-    echo "O JOGO DUROU $duracao HORA(S) E 0 MINUTO(S)\n";
+if ($total_minutos < 0) {
+    $total_minutos = 60 + ($total_minutos);
+    $total--;
+}
+
+if ($total == 24 && $total_minutos > 0) {
+    $total = 0;
 }
 
 if ($inicio == $final && $inicio_minutos == $final_minutos) {
     echo "O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)\n";
-} 
-
-if ($inicio > $final){
-    $duracao = ($inicio - $final);
-    $duracao_minutos = ($inicio_minutos - $final_minutos);
-    echo "O JOGO DUROU $duracao HORA(S) E $duracao_minutos MINUTO(S)\n";
+} else {
+    echo "O JOGO DUROU $total HORA(S) E $total_minutos MINUTO(S)\n";
 }
