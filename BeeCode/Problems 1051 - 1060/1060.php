be01d44e91cb91cf1ea062018b024f93219c1538
Gabriel Nascimento de Souza <84488487+Dev-GabrielN@users.noreply.github.com>
2022-12-05 10:18:51 -0300

    solved problems

---
+++ BeeCode/Problems 1051 - 1060/1060.php
@@ -0,0 +1,30 @@
+<?php
+
+$num_1 = (float)readline();
+$num_2 = (float)readline();
+$num_3 = (float)readline();
+$num_4 = (float)readline();
+$num_5 = (float)readline();
+$num_6 = (float)readline();
+$valor = 0;
+
+if ($num_1 > 0) {
+    $valor = $valor + 1;
+}
+if ($num_2 > 0) {
+    $valor = $valor + 1;
+}
+if ($num_3 > 0) {
+    $valor = $valor + 1;
+}
+if ($num_4 > 0) {
+    $valor = $valor + 1;
+}
+if ($num_5 > 0) {
+    $valor = $valor + 1;
+}
+if ($num_6 > 0) {
+    $valor = $valor + 1;
+}
+
+echo "$valor valores positivos\n";
