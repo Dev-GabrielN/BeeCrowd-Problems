dea092f191e886c38b20faeda173c07e115d7fa9
Gabriel Nascimento de Souza <84488487+Dev-GabrielN@users.noreply.github.com>
2023-01-25 10:06:04 -0300

    update

--- BeeCode/Problems 1141 - 1150/1149.php
+++ BeeCode/Problems 1141 - 1150/1149.php
@@ -1,2 +1,27 @@
 <?php
+/*
+Faça um algoritmo para ler um valor A e um valor N. Imprimir a soma de A + i para cada i com os valores (0 <= i <= N-1). Enquanto N for negativo ou ZERO, um novo N(apenas N) deve ser lido.
 
+Entrada
+A entrada contém somente valores inteiros, podendo ser positivos ou negativos. Todos os valores estão na mesma linha.
+
+Saída
+A saída contém apenas um valor inteiro.
+*/
+$entrada = readline();
+$entrada = explode(' ', $entrada);
+
+$a = intval($entrada[0]);
+$n = intval($entrada[1]);
+$j = 2;
+$result = 0;
+while ($n <= 0) {
+    $n = intval($entrada[$j]);
+    $j++;
+}
+$i = $n - 1;
+
+for ($x = 0; $x <= $i; $i--) {
+    $result = $result + ($a + $i);
+}
+echo "$result\n";
